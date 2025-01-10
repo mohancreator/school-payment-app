4e03c8a531e3cec877625021ad64364832bb16d3
Mohan <madhiazagan2002@gmail.com>
2025-01-10 19:51:04 +0530

    Update frontend

---
+++ school-payment-frontend/src/Components/Home/index.js
@@ -0,0 +1,15 @@
+
+import Navbar from "../Navbar"
+
+const Home = () => {
+    return (
+        <div>
+            <Navbar/>
+            <div>
+                <h1>Transaction</h1>
+            </div>
+        </div>
+    )
+}
+
+export default Home
---
+++ school-payment-frontend/src/Components/Navbar/index.js
@@ -0,0 +1,12 @@
+import './index.css'
+
+const Navbar = () => {
+    return (
+        <Navbar>
+            <h1>History</h1>
+            <button>Export</button>
+        </Navbar>
+    )
+}
+
+export default Navbar
