abd0fed0c9b43e96d2451b7738805dc4d63c6c9f
Mohan <madhiazagan2002@gmail.com>
2025-01-12 19:14:53 +0530

    update backend

--- school-payment-backend/api/index.js
+++ school-payment-backend/api/index.js
@@ -47,7 +47,7 @@ app.get('/transactions', async (req, res) => {
   try {
     const db = mongoose.connection.db
     const collectRequestStatus = db.collection('collect_request_status')
-    const transactions = await collectRequestStatus.find({}).toArray();
+    const transactions = await collectRequestStatus.find();
 
     res.status(200).json({ success: true, data: transactions });
   } catch (err) {
