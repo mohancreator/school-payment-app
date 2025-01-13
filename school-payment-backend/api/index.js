2c793642515f0bf57b76eb74115306d1bfbc600f
Mohan B <116074325+mohancreator@users.noreply.github.com>
2025-01-13 11:04:24 +0530

    Update index.js

--- school-payment-backend/api/index.js
+++ school-payment-backend/api/index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const dotenv = require('dotenv');
 const mongoose = require('mongoose');
 const cors = require('cors');
-const bodyParser = require('body-parser');
+
 
 dotenv.config();
 
